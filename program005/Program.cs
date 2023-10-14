fec655a41423e06789348f684c3bedd78d8c2e6e
Zpitoiti <2008dima15@gmail.com>
2023-10-14 19:55:42 +0700

    коммит  program006

--- program005/Program.cs
+++ program005/Program.cs
@@ -1,4 +1,12 @@
 ﻿Console.Write("Введите имя пользователя");
 string username = Console.ReadLine();
 
-if (username == "Маша")
+if(username.ToLower() == "Маша")
+{
+    Console.WriteLine("Неужели , это же МАШААААААААААА!!!!!!!!!!");
+}
+else 
+{
+    Console.Write("СаламалейКУУМ, ");
+    Console.WriteLine(username);
+}
