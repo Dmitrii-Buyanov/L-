ec7ce55d5839d67a6071016d638d88d302489d7e
Zpitoiti <2008dima15@gmail.com>
2023-10-16 18:20:46 +0700

    program007

--- program003/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿int NumberA = new Random().Next(1 ,10) ;
-Console.WriteLine(NumberA);
-int NumberB = new Random().Next(1 ,10) ;
-Console.WriteLine(NumberB);
-Console.WriteLine(NumberA + NumberB);
--- program004/Program.cs
+++ program004/Program.cs
@@ -1,4 +1,1 @@
-﻿double NumberA = 12;
-double NumberB =  5;
-Console.WriteLine(NumberA / NumberB);
-
+﻿
